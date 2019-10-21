d0228d194a81ec6d6fe723fb59b3523bbc707a78
Worabodee <neua.worabodee@gmail.com>
2019-10-21 22:30:02 +0700

    fixed speed slider

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -39,6 +39,7 @@ class Toolbar extends Component {
             type="range"
             min="1"
             max="10"
+            value="5.5"
             disabled={this.props.sorting}
             onChange={this.handleSpeedChange}
           />
